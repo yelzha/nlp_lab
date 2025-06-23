15224ce9ba3ce5a79e58e2b33450b1b8c14f45f0
yelzha <54392243+yelzha@users.noreply.github.com>
2025-06-23 19:16:07 +0200

    test fix

--- run_code.sh
+++ run_code.sh
@@ -16,13 +16,13 @@ ollama run qwen3:0.6b || true
 module load Miniforge3
 module load git/2.41.0-GCCcore-12.3.0-nodocs
 
-#conda create -n /env python=3.11 numpy pandas -y
+#conda create -p /home/s06zyelt/nlp_lab/env python=3.10 -y
 source /software/easybuild-INTEL_A40/software/Miniforge3/24.1.2-0/etc/profile.d/conda.sh
 conda activate /home/s06zyelt/nlp_lab/env
 
-pip install numpy pandas
-pip install openai sacrebleu
-pip install git+https://github.com/openai/human-eval.git
+#pip install numpy pandas
+#pip install openai sacrebleu
+#pip install git+https://github.com/openai/human-eval.git
 
 cd AgentForest/script
 sh run_experiments.sh
--- run_test.sh
+++ run_test.sh
@@ -8,13 +8,13 @@ module load Miniforge3
 module load git/2.41.0-GCCcore-12.3.0-nodocs
 
 
-# conda create -n /env python=3.11 numpy pandas -y
+#conda create -p /home/s06zyelt/nlp_lab/env python=3.10 -y
 source /software/easybuild-INTEL_A40/software/Miniforge3/24.1.2-0/etc/profile.d/conda.sh
 conda activate /home/s06zyelt/nlp_lab/env
 
-pip install numpy pandas
-pip install openai sacrebleu
-pip install git+https://github.com/openai/human-eval.git
+#pip install numpy pandas
+#pip install openai sacrebleu
+#pip install git+https://github.com/openai/human-eval.git
 
 python -c "import numpy, pandas, openai; print('All good')"
 
