68f7912c315e3ffb63aee515dc9c12464223c8fd
yelzha <54392243+yelzha@users.noreply.github.com>
2025-07-05 22:16:08 +0200

    from vllm import LLM, SamplingParams

--- vllm_test.py
+++ vllm_test.py
@@ -45,50 +45,50 @@
 
 
 
-# NOTE: remember to run: pip install openai
-from openai import OpenAI
-import os
-
-llm_ip = os.getenv('LLM_IP')
-
-openai_api_key = "EMPTY"
-openai_api_base = f"http://{llm_ip}/v1"
-
-client = OpenAI(
-    api_key=openai_api_key,
-    base_url=openai_api_base,
-)
-completion = client.completions.create(
-    model="Qwen/Qwen3-4B",
-    prompt=["A robe takes 2 bolts of blue fiber and half that much white fiber. How many bolts in total does it take?"],
-    n=5,
-    max_tokens=1024)
-print(completion)
-print("\n\n\n")
-print(completion.choices)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from vllm import LLM, SamplingParams
-# prompts = [
-#     "Mexico is famous for ",
-#     "The largest country in the world is ",
-# ]
+# from openai import OpenAI
+# import os
 #
-# sampling_params = SamplingParams(temperature=0.7, top_p=0.9)
-# llm = LLM(model="Qwen/Qwen3-4B")
-# responses = llm.generate(prompts, sampling_params)
+# llm_ip = os.getenv('LLM_IP')
 #
-# for response in responses:
-#     print(response.outputs[0].text)
+# openai_api_key = "EMPTY"
+# openai_api_base = f"http://{llm_ip}/v1"
+#
+# client = OpenAI(
+#     api_key=openai_api_key,
+#     base_url=openai_api_base,
+# )
+# completion = client.completions.create(
+#     model="Qwen/Qwen3-4B",
+#     prompt=["A robe takes 2 bolts of blue fiber and half that much white fiber. How many bolts in total does it take?"],
+#     n=5,
+#     max_tokens=1024)
+# print(completion)
+# print("\n\n\n")
+# print(completion.choices)
+
+
+
+
+
+
+
+
+
+
+
+
+
+from vllm import LLM, SamplingParams
+prompts = [
+    "A robe takes 2 bolts of blue fiber and half that much white fiber. How many bolts in total does it take?",
+    "A robe takes 2 bolts of blue fiber and half that much white fiber. How many bolts in total does it take?",
+]
+
+sampling_params = SamplingParams(temperature=0.7, top_p=0.9)
+llm = LLM(model="Qwen/Qwen3-4B")
+responses = llm.generate(prompts, sampling_params)
+
+for response in responses:
+    for i in response.outputs:
+        print(i.text)
 
