a90adee2fbaf507008e5898cff2448e85e57746a
yelzha <54392243+yelzha@users.noreply.github.com>
2025-07-18 18:05:18 +0200

    Update utils.py

--- AgentForestRefactored/src/utils.py
+++ AgentForestRefactored/src/utils.py
@@ -46,10 +46,10 @@ def get_mmlu_qa_pairs(df, ix):
     b = df.iloc[ix, 2]
     c = df.iloc[ix, 3]
     d = df.iloc[ix, 4]
-    subject = df.iloc[ix, "name"]
+    answer = df.iloc[ix, 5]
+    subject = df.iloc[ix, 6]
     question = interaction_prompt["mmlu"]["question"].format(
         subject, question, a, b, c, d)
-    answer = df.iloc[ix, 5]
     return question, answer
 
 
