10fab92750f03d1fcdb901ac0e01888ecfe65a9c
yelzha <54392243+yelzha@users.noreply.github.com>
2025-08-09 15:22:25 +0200

    Update test.sh

--- R2ATA/test.sh
+++ R2ATA/test.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+
 nvidia-smi
 python -c "import torch,os; print('cuda_avail=', torch.cuda.is_available(), 'num=', torch.cuda.device_count(), 'vis=', os.environ.get('CUDA_VISIBLE_DEVICES'))"
